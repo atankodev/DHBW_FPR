a4dd2b68c86f5339f13df10ad352f7a4e1617ca8
adriantanko <adriantanko@adrians-air.fritz.box>
2022-03-08 22:15:02 +0100

    commit

--- Test/src/SampleJFrame.java
+++ Test/src/SampleJFrame.java
@@ -1,8 +1,29 @@
+import java.awt.*;
+import javax.swing.*;
 
-public class SampleJFrame {
+public class SampleJFrame extends JFrame {
+	
+	Container c;
+	JLabel l;
+	
+	public SampleJFrame() {
+		
+		c = getContentPane();
+		c.setLayout(new FlowLayout());
+		
+		l = new JLabel("Hello JFrame!");
+		
+		c.add(l);
+		
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		SampleJFrame sampleJFrame = new SampleJFrame();
+		sampleJFrame.setSize(720, 480);
+		sampleJFrame.setTitle("My first JFrame!");
+		sampleJFrame.setVisible(true);
+		sampleJFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
 
 	}
 
