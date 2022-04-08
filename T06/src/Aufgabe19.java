4c9fbe7b9c3287c6549a259731d283afbc88f23b
adriantanko <adriantanko@localhost>
2022-04-08 16:42:16 +0200

    commit

--- T06/src/Aufgabe19.java
+++ T06/src/Aufgabe19.java
@@ -4,20 +4,26 @@ public class Aufgabe19 {
 
 	public static void main(String[] args) throws IOException {
 		
-		File inputFile = new File("Eingabe.txt");
-		File outputFile = new File("Ausgabe.txt");
+		File inputFile = new File("/Users/adriantanko/git/DHBW_FPR/T06/src/Eingabe.txt");
+		File outputFile = new File("/Users/adriantanko/git/DHBW_FPR/T06/src/Ausgabe.txt");
 			
 		FileReader fileReader = new FileReader(inputFile);
 		FileWriter fileWriter = new FileWriter(outputFile);
 		
 		int c;
 		
+		while((c = fileReader.read()) != (int) '\n') {
+			fileWriter.write(c);
+		}
+		
+		fileWriter.write("NEUE ZEILE");
+		fileWriter.write("\n");
+		
 		while((c = fileReader.read()) != -1) {
 			fileWriter.write(c);
 		}
 		
 		fileReader.close();
-		fileWriter.write("NEUE ZEILE");
 		fileWriter.close();
 		
 	}
